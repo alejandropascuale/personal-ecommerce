7a4b26e82da0f17fdb2415d832d5d4bb4cacdb17
alejandropascuale <apascuale@outlook.com>
2022-04-17 20:47:30 -0300

    create BoxProduct components with sample data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import BoxProduct from './components/products/BoxProduct';
 
 function App() {
   return (
     <>
       <Header />
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {/* <BoxProduct /> */}
     </>
   );
 }
