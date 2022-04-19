22e1c4579cc585fd9cb1f8a9cf0fb3e0201eb1b0
alejandropascuale <apascuale@outlook.com>
2022-04-19 11:17:33 -0300

    add extension to BoxProduct component

--- src/components/products/BoxProduct.jsx
+++ src/components/products/BoxProduct.jsx
@@ -1,30 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './boxProduct.css';
 
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  // More products...
-]
-
 export default function BoxProduct() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const apiProducts = await axios.get('http://localhost:8000/api/products');
+    setProducts(apiProducts.data);
+  }
+  
+  useEffect(() => {
+    fetchProducts();
+    console.log(products);
+  }, []);
+
   return (
-    <div className="bg-white">
+    <>
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div key={product.id} className="group relative">
+            <div key={product.idPlato} className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
-                  src={product.imageSrc}
+                  src={product.imagen}
                   alt={product.imageAlt}
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
@@ -32,9 +33,9 @@ export default function BoxProduct() {
               <div className="mt-4 flex justify-between">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+                    <a href=''>
                       <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
+                      {product.plato}
                     </a>
                   </h3>
                   <p className="mt-1 text-sm text-gray-500">{product.color}</p>
@@ -45,6 +46,6 @@ export default function BoxProduct() {
           ))}
         </div>
       </div>
-    </div>
+    </>
   )
 }
