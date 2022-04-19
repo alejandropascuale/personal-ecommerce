786010a83af9f4b6e0d09b26a1d299a7364d0001
alejandropascuale <apascuale@outlook.com>
2022-04-19 16:06:34 -0300

    add new alt to images and slice products to 8

--- src/components/products/BoxProduct.jsx
+++ src/components/products/BoxProduct.jsx
@@ -18,14 +18,13 @@ export default function BoxProduct() {
     <>
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
-
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
+          {products.slice(0,8).map((product) => (
             <div key={product.idPlato} className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
                   src={product.imagen}
-                  alt={product.imageAlt}
+                  alt={`${product.plato}-${product.idPlato}`}
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
               </div>
