9f979637358f63dff6270cdc000469394c4d7ff6
alejandropascuale <apascuale@outlook.com>
2022-04-19 17:15:00 -0300

    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Header from './components/header/Header';
+import LoginRegister from './components/login-register/LoginRegister';
 import BoxProduct from './components/products/BoxProduct';
 
 function App() {
   return (
-    <>
-      <Header />
-      <BoxProduct />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<BoxProduct />} />
+        <Route path="/sign" element={<LoginRegister />} />
+      </Routes>
+    </Router>
   );
 }
 
